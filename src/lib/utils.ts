ee222ee542b81ffe44560a21196bbfe67cb3a933
Rene-Nun <christiannuntkd@hotmail.com>
2026-03-20 17:17:16 -0600

    Add files via upload

---
+++ src/lib/utils.ts
@@ -0,0 +1,27 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatMXN(amount: number) {
+  return new Intl.NumberFormat("es-MX", {
+    style: "currency",
+    currency: "MXN",
+    minimumFractionDigits: 0,
+  }).format(amount);
+}
+
+export function validateMexicanPhone(phone: string) {
+  return /^[0-9]{10}$/.test(phone.replace(/\s/g, ""));
+}
+
+export function calculateAge(birthdate: string): number {
+  const today = new Date();
+  const birth = new Date(birthdate);
+  let age = today.getFullYear() - birth.getFullYear();
+  const m = today.getMonth() - birth.getMonth();
+  if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) age--;
+  return age;
+}
